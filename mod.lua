62061ac0aa44baf36249b0ef8de5e154a517a224
Daniel Reichelt <dr1400@web.de>
2020-03-28 10:50:10 +0100

    Initial commit

---
+++ mod.lua
@@ -0,0 +1,22 @@
+function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "NONE",
+		name = _("Terrain Fever"),
+		visible = false,
+		description = _([[Adds a temperate terrain generator written from scratch.
+
+Terrain Fever features various terrain types that you can combine using a selectable distribution type which allows for great flexibility. Create plains, hills, coast lines, islands, huge and small lakes or set yourself up for a mountainous challenge.
+		]]),
+		tags = { "Map", "Terrain", "Generator", "Generation", "Temperate", "European", "Hills", "Mountains", "Lakes", "Ocean", "Sea", "Misc" },
+		authors = {
+			{
+				name = "Haviland",
+				role = 'CREATOR',
+			},
+		},
+	}
+}
+end
